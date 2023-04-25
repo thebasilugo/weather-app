4447e98b2e435c1e10fd07bf0b83d4fc696f5fee
joseph shodunke <josephshodunke4@gmail.com>
2023-04-25 22:06:01 +0100

    Update views.py

--- testhost/testapi/views.py
+++ testhost/testapi/views.py
@@ -2,26 +2,25 @@ from django.shortcuts import render
 import requests
 
 def get_lon_lat(place):
-    url = "https://maps.googleapis.com/maps/api/geocode/json"
+    url = "https://api.opencagedata.com/geocode/v1/json"
     params = {
-        "address": place,
-        "key": "YOUR_API_KEY"
-    }
+    "q" : "Nigeria,abeokuta",
+    "key": "1bf7cd2a2df44832b4b3ef23d2a0424c",
+}
     response = requests.get(url, params=params)
     data = response.json()
-    if data['status'] == 'OK':
-        location = data['results'][0]['geometry']['location']
-        return location['lng'], location['lat']
-    else:
-        return None
-
+    
+    lat = data['results'][0]['geometry']['lat']
+    lng = data['results'][0]['geometry']['lng']
+    return lat , lng
+    
 # Create your views here.
 def home(request):
     if request.method == 'POST':
         country = request.POST.get('country')
         city = request.POST.get('city')
         place = f"{country}, {city}"
-        lon, lat = get_lon_lat(place)
+        lat, lon = get_lon_lat(place)
 
         para = {
             "lon": lon,
@@ -31,8 +30,10 @@ def home(request):
 
         connection = requests.get("http://api.openweathermap.org/data/2.5/weather", params=para)
         data = connection.json()
+        print(f'the data {data}')
+        return render(request, "index.html", {'data': data})
         
-        
+
 
     return render(request, "index.html")
 
