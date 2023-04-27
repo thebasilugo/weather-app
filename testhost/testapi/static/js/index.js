d5b0555f8962851f5f9de5f3d74eba873b7372b5
joseph shodunke <josephshodunke4@gmail.com>
2023-04-27 14:59:02 +0100

    Add files via upload
    adding the javascript folder and file

---
+++ testhost/testapi/static/js/index.js
@@ -0,0 +1,10 @@
+// Display names at the home page
+
+let theBoys = document.getElementById("the-boys");
+theBoys.textContent = "Ugiee and Josef's";
+
+// Display the weather information when the submit button is clicked
+let displayInfo = document.getElementById("display-info");
+function showWeatherInfo() {
+    displayInfo.textContent = "Weather Info displayed here!";
+}
