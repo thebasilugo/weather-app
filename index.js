861b4af17c704af9639f9c3b4d4fed9fa2f626c6
Basil, Ugo <115357283+thebasilugo@users.noreply.github.com>
2023-04-28 20:30:14 +0000

    Update index.js
    addition of 'show' and 'hide' button changes, visualization of supposed weather information when read from source

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-// Display names at the home page
+const displayinfo = document.querySelector("#display-info");
+const btn = document.querySelector("#weather-info");
 
-let theBoys = document.getElementById("the-boys");
-theBoys.textContent = "Ugiee and Josef's";
-
-// Display the weather information when the submit button is clicked
-let displayInfo = document.getElementById("display-info");
-function showWeatherInfo() {
-    displayInfo.textContent = "Weather Info displayed here!";
+function displayInfo() {
+    if (displayinfo.className == "show-weather heightened") {
+        // Hide weather details
+        displayinfo.className = "no-heightened";
+        displayinfo.textContent = "";
+        btn.innerHTML = "Show Weather";
+    } else {
+        //Show weather details
+        displayinfo.className = "show-weather heightened";
+        displayinfo.textContent = "Bleh, This is where the weather would show.";
+        btn.innerHTML = "Hide Weather";
+    }
 }
